130892659f3fdf5901c2626bb1a3367f391972d3
Alina Zhdanova <alina@electrontogo.com>
2020-03-05 21:18:08 +0300

    .

---
+++ lesson1/src/Main.java
@@ -0,0 +1,9 @@
+package ru.gb.jtwo.la.online;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        MyClass m = new MyClass();
+        m.method();
+    }
+}
---
+++ lesson1/src/MyClass.java
@@ -0,0 +1,12 @@
+package ru.gb.jtwo.la.online;
+
+public class MyClass {
+    int a = 10;
+
+    void method() {
+        int i = 10;
+    }
+
+    public MyClass() {
+    }
+}
